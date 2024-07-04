c22c2206d317b47042d1c86f7917b2d420645fcb
Nils Wild <nils.wild@swc.rwth-aachen.de>
2024-07-04 11:05:09 +0200

    fix: added contextual serializer and deserializer for RestMessages

--- interact-domain/src/main/kotlin/de/interact/domain/rest/RestMessage.kt
+++ interact-domain/src/main/kotlin/de/interact/domain/rest/RestMessage.kt
@@ -1,14 +1,21 @@
 package de.interact.domain.rest
 
+import arrow.core.raise.catch
 import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer
+import com.fasterxml.jackson.databind.jsontype.TypeDeserializer
+import de.interact.domain.serialization.SerializationConstants
+import io.github.projectmapk.jackson.module.kogera.readValue
 
 sealed interface RestMessage<T> {
     val path: String
     val parameters: Map<String, String>
     val headers: Map<String, String>
+    @get:JsonDeserialize(using = BodyDeserializer::class)
     @get:JsonSerialize(using = BodySerializer::class)
     val body: T?
 
@@ -31,9 +38,35 @@ sealed interface RestMessage<T> {
 class BodySerializer : JsonSerializer<Any>() {
     override fun serialize(value: Any, gen: JsonGenerator, serializers: SerializerProvider) {
         if(value is String) {
-            gen.writeString(value)
+            if(value.isEmpty()) {
+                gen.writeString("")
+            } else {
+                catch({
+                    SerializationConstants.mapper.readTree(value)
+                    gen.writeRawValue(value)
+                }) {
+                    gen.writeString(value)
+                }
+            }
         }else {
-            gen.writeObject(value)
+            SerializationConstants.messageMapper.serializerFactory.createSerializer(
+                serializers,
+                SerializationConstants.messageMapper.typeFactory.constructType(value::class.java)).serialize(value, gen, serializers)
         }
     }
+}
+
+class BodyDeserializer : JsonDeserializer<Any>(), ContextualDeserializer {
+
+    private var valueType: JavaType? = null
+
+    override fun createContextual(ctx: DeserializationContext, property: BeanProperty?): JsonDeserializer<*> {
+        val bodyType = property?.type ?: ctx.contextualType
+        return BodyDeserializer().apply { valueType = bodyType }
+    }
+
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Any {
+        return SerializationConstants.messageMapper.readValue(p, valueType)
+    }
+
 }
